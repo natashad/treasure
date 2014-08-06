4fcc8c5b0110b56bc60fe8a8e3e7207f657826ae
Natasha Dalal <natashadalal@gmail.com>
2014-08-05 21:21:09 -0400

    adding a grid of navigable tiles

--- app/src/main/java/endee/fried/treasure/MyView.java
+++ app/src/main/java/endee/fried/treasure/MyView.java
@@ -7,54 +7,80 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 /**
  * Created by natasha on 2014-08-05.
  */
 public class MyView extends SurfaceView {
 
-    private int radius = 200;
-    private boolean circleTouched = false;
+    private int radius = 70;
+    private MapLocation[][] locations = new MapLocation[5][5];
+//    An integer array storing the x and y of currently active location
+    private int[] currentlyActive = new int[2];
 
     public MyView(Context context) {
         super(context);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                locations[i][j] = new MapLocation((i*radius*2)+radius, (j*radius*2)+radius, radius);
+            }
+        }
+        currentlyActive[0] = 0;
+        currentlyActive[1] = 0;
+        locations[currentlyActive[0]][currentlyActive[1]].setActive(true);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setColor(circleTouched ? Color.GREEN : Color.RED);
-        canvas.drawCircle(getWidth()/2, getHeight()/2, radius, paint);
+        for (int i = 0; i < locations.length; i++) {
+            for (int j = 0; j < locations[i].length; j++) {
+                paint.setColor(locations[i][j].isActive() ? Color.GREEN : Color.RED);
+                canvas.drawCircle(locations[i][j].getX(), locations[i][j].getY(), radius, paint);
+            }
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float touchX = event.getX();
         float touchY = event.getY();
-        if (Math.pow((touchX - getWidth()/2), 2) + Math.pow((touchY - getHeight()/2), 2) < Math.pow(radius,2)) {
-            System.out.println("CIRCLE");
-            int eventAction = event.getAction();
+        boolean validTouch = false;
 
-            switch (eventAction) {
-                case MotionEvent.ACTION_DOWN:
-                    circleTouched = true;
-                    break;
+        int eventAction = event.getAction();
 
-                case MotionEvent.ACTION_MOVE:
-                    // finger moves on the screen
-                    break;
+        switch (eventAction) {
+            case MotionEvent.ACTION_DOWN:
 
-                case MotionEvent.ACTION_UP:
-                    circleTouched = false;
-                    break;
-            }
-        }
-        else {
-            circleTouched = false;
-            System.out.println(Math.pow((touchX - getWidth()/2), 2));
-            System.out.println(Math.pow((touchY - getHeight()/2), 2));
-            System.out.println(Math.pow(radius, 2));
+                ArrayList<int[]> neighbours = new ArrayList<int[]>();
+                neighbours.add(new int[]{currentlyActive[0] + 1, currentlyActive[1]});
+                neighbours.add(new int[]{currentlyActive[0] - 1, currentlyActive[1]});
+                neighbours.add(new int[]{currentlyActive[0], currentlyActive[1] + 1});
+                neighbours.add(new int[]{currentlyActive[0], currentlyActive[1] - 1});
 
+                for (int i = 0; i < neighbours.size(); i++) {
+                    int x = neighbours.get(i)[0];
+                    int y = neighbours.get(i)[1];
+                    if (x >= 0 && x < locations.length && y >= 0 && y < locations.length) {
+                        if (locations[x][y].isInBounds(touchX, touchY)) {
+                            locations[currentlyActive[0]][currentlyActive[1]].setActive(false);
+                            currentlyActive[0] = x;
+                            currentlyActive[1] = y;
+                            break;
+                        }
+                    }
+                }
+                locations[currentlyActive[0]][currentlyActive[1]].setActive(true);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                // finger moves on the screen
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
         }
         this.invalidate();
         return true;
